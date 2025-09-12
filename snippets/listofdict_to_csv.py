07c2e36d469e33ee94750596ff1a45c79c142dc9
Avnish <avnish.singh@cuculus.in>
2025-09-12 12:21:22 +0530

    organised the structure

--- snippets/listofdict_to_csv.py
+++
@@ -1,8 +0,0 @@
-def csv_to_lod(filepath):
-    dev_list = [] 
-    with open(filepath, mode='r') as file:
-        csv_reader = csv.DictReader(file)
-        for row in csv_reader:
-            row_dict = dict(row)  
-            dev_list.append(row_dict)
-    return dev_list
